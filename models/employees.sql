ff0b037f8bd58972f75a61183f9ac0c0f09909a9
Karen Garbim <karenkelly.g@tcs.com>
2025-05-07 10:22:13 +0000

    version atualizada da main

--- models/employees.sql
+++ models/employees.sql
@@ -1,1 +1,12 @@
-select * from {{source('sources','employees')}};
+with calc_employees as(
+
+select 
+
+date_part(year, current_date) - date_part(year, birth_date) age,
+date_part(year, current_date) - date_part(year, hire_date) lengthofservice,
+first_name ||' '|| last_name name, 
+*
+from {{source('sources','employees')}}
+)
+
+select * from calc_employees
